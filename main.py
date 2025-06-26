ce4331e0bb034fda894c3baa4045484ce20a5e16
SVET <137564450+FastGEUS@users.noreply.github.com>
2025-06-26 15:11:15 +0300

    Hot Fix Pacth

--- main.py
+++ main.py
@@ -4,4 +4,12 @@ from app import BegemotArtilleryCalculator
 if __name__ == "__main__":
     root = tk.Tk()
     app = BegemotArtilleryCalculator(root)
+    
+    # Добавляем обработчик корректного закрытия окна
+    def on_closing():
+        root.destroy()
+        root.quit()
+    
+    root.protocol("WM_DELETE_WINDOW", on_closing)
+    
     root.mainloop()
